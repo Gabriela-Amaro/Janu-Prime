001e6b88790bb59989c61dfcea87aaf71235a331
Gabi <gads12@aluno.ifnmg.edu.br>
2025-09-28 00:03:47 -0300

    adiciona created_at e update_at na tabela usuarios

--- app/januprime-backend/app/usuarios/models.py
+++ app/januprime-backend/app/usuarios/models.py
@@ -46,6 +46,9 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     objects = UsuarioManager()
 
     USERNAME_FIELD = "email"
