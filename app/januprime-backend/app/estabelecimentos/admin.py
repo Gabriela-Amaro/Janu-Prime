b66d595ffdbe857ac06848121f84247b48954ffb
Gabi <gads12@aluno.ifnmg.edu.br>
2025-10-21 21:37:43 -0300

    Adiciona submódulos do frontend e backend

--- app/januprime-backend/app/estabelecimentos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Estabelecimento
-
-@admin.register(Estabelecimento)
-class EstabelecimentoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'cnpj', 'endereco', 'telefone', 'ativo', 'created_at', 'updated_at')
-    list_filter = ('ativo',)
-    search_fields = ('nome', 'cnpj')
-    ordering = ('created_at', 'nome')
